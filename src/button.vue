949e4833167201c79c2e30d1146a3c47bd9558b2
Mr.X <397906946@qq.com>
2019-03-02 18:57:34 +0800

    将icon代码整理成icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <svg v-if="icon" class="icon">
-      <use :xlink:href="`#i-${icon}`"></use>
-    </svg>
+    <g-icon v-if="icon" :name="icon"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -11,7 +9,16 @@
 
 <script>
   export default {
-    props: ['icon', 'iconPosition']
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(value){
+          return value === 'left' || value === 'right'
+        }
+      }
+    }
   }
 </script>
 
