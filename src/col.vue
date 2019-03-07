95b2623c64000c3e1f9bd8410c364716360baeea
Mr.X <397906946@qq.com>
2019-03-07 16:49:45 +0800

    基本实现网格系统功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]"
+  :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}">
+    <div style="border: 1px solid green; height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
@@ -9,21 +12,35 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      }
+    },
+    data(){
+      return {
+        gutter: {
+          type: [Number, String]
+        }
       }
     }
   }
 </script>
 <style scoped lang="scss">
   .col {
-    height: 100px;
-    background-color: grey;
     width: 50%;
-    border: 1px solid red;
 
-    $class: col-;
+    $class-prefix: col-;
     @for $n from 1 through 24 {
-      &.#{$class}#{$n} {
+      &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
