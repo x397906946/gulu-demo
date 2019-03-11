8569e890901919cb166a4f05daee3dd001f73c96
Mr.X <397906946@qq.com>
2019-03-11 16:42:29 +0800

    添加 tabs 测试用例

--- src/tabs.vue
+++ src/tabs.vue
@@ -34,7 +34,7 @@
     mounted(){
       if(this.$children.length === 0) {
         console && console.warn &&
-        console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
+        console.warn('tabs的子组件应该是tabs-head和tabs-body，但你没有写子组件')
       }
       this.$children.forEach((vm) => {
         if(vm.$options.name === 'GuluTabsHead') {
