4f9a3ab90244bd300002b66c6cfd5e7cbc037dfd
Mr.X <397906946@qq.com>
2019-03-13 12:14:42 +0800

    完成 collapse 基本样式与功能

---
+++ src/collapse.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="collapse">
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GuluCollapse"
+  }
+</script>
+
+<style scoped lang="scss">
+  $grey: #ddd;
+  $border-radius: 4px;
+  .collapse {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+  }
+</style>
